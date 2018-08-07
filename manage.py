14667262838046c35512646b191ee266116ab86a
Christopher Wolfe <noone234@gmail.com>
2018-08-06 21:08:15 -0400

    Reorganized source code.

--- manage.py
+++ manage.py
@@ -9,11 +9,9 @@ __maintainer__ = "Christopher Wolfe"
 __email__ = "noone234@gmail.com"
 __status__ = "Development"
 
-from flask_script import Manager
 from app import create_app
 
 app = create_app()
-manager = Manager(app)
 
 if __name__ == "__main__":
     app.run(
